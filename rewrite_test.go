aa8fb6cf9371484afda08fc6f7adb59df45319f4
sfomuseumbot <sfomuseumbot@localhost>
2024-08-09 11:31:35 -0700

    improved tests

--- rewrite_test.go
+++ rewrite_test.go
@@ -2,6 +2,7 @@ package uri
 
 import (
 	"context"
+	"net/url"
 	"testing"
 )
 
@@ -9,11 +10,12 @@ func TestRewriteURI(t *testing.T) {
 
 	ctx := context.Background()
 
-	candidates := []string{
-		"rewrite:///tmp/bob.jpg?target=alice.jpg",
+	candidates := map[string]string{
+		"rewrite:///tmp/bob.jpg?target=alice.jpg":                            "rewrite:///tmp/bob.jpg?target=alice.jpg",
+		"rewrite:///usr/local/images/99400/99481/99481.jpg?target=99481.jpg": "rewrite:///usr/local/images/99400/99481/99481.jpg?target=99481.jpg",
 	}
 
-	for _, str_uri := range candidates {
+	for str_uri, expected_str := range candidates {
 
 		u, err := NewURI(ctx, str_uri)
 
@@ -21,7 +23,7 @@ func TestRewriteURI(t *testing.T) {
 			t.Fatalf("Failed to create new IIIF URI for '%s', %v", str_uri, err)
 		}
 
-		if u.String() != "rewrite:///tmp/bob.jpg?target=alice.jpg" {
+		if u.String() != expected_str {
 			t.Fatalf("Unexpected string value for '%s': '%s'", str_uri, u.String())
 		}
 
@@ -31,7 +33,15 @@ func TestRewriteURI(t *testing.T) {
 			t.Fatalf("Unable to determine target for '%s', %v", str_uri, err)
 		}
 
-		if target != "alice.jpg" {
+		ru, err := url.Parse(str_uri)
+
+		if err != nil {
+			t.Fatalf("Failed to parse URI, %v", err)
+		}
+
+		rq := ru.Query()
+
+		if target != rq.Get("target") {
 			t.Fatalf("Unexpected target for '%s': '%s'", str_uri, target)
 		}
 	}
